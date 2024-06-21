f270542c3e269b085b1c6df88a249a1ceca5d235
Shashank Kumar <39635214+sneakatyou@users.noreply.github.com>
2024-06-21 23:22:45 +0530

    Changes in executor

--- agent_torch/executor.py
+++ agent_torch/executor.py
@@ -3,26 +3,18 @@ import pdb
 import sys
 
 from agent_torch.dataloader import DataLoader
-
+from agent_torch.runner import Runner
+from tqdm import trange
 class BaseExecutor():
     def __init__(self, model):
         self.model = model
     
     def _get_runner(self, config):
-        module_name = f'{self.model.__name__}.simulator'
+        module_name = f'{self.model.__name__}.main'
         module = importlib.import_module(module_name)
-        simulaton_runner = module.SimulationRunner
-        registry = self._get_registry()
-        runner = simulaton_runner(config, registry)
+        registry = module.registry
+        runner = Runner(config, registry)
         return runner
-    
-    def _get_registry(self):
-        module_name = f'{self.model.__name__}.simulator'
-        module = importlib.import_module(module_name)
-        simulation_registry = module.simulation_registry
-        sys.path.insert(0, self.model.__path__[0])
-        registry = simulation_registry()
-        return registry
     
 class Executor(BaseExecutor):
     def __init__(self,model,data_loader=None,pop_loader=None) -> None: 
@@ -34,15 +26,25 @@ class Executor(BaseExecutor):
             self.data_loader = data_loader
         
         self.config = self.data_loader.get_config()
+        self.runner = self._get_runner(self.config)
+
+    def init(self,opt):
+        self.runner.init()
+        self.learnable_params = [param for param in self.runner.parameters() if param.requires_grad]
+        self.opt = opt(self.learnable_params)
+    
+    def execute(self,key,num_episodes = None,num_steps_per_episode = None):
+        for episode in trange(num_episodes):
+            self.opt.zero_grad()
+            self.runner.reset()
+            self.runner.step(num_steps_per_episode)
+        self.simulation_values = self.runner.get_simulation_values(key)
+    
+    def get_simulation_values(self,key,key_type = 'environment'):
+        self.simulation_values = self.runner.state_trajectory[-1][-1][key_type][key] # List containing values for each step
+        return self.simulation_values
+
         
-        # self.runner = self._get_runner(self.config)
-        # self.runner.init()
-        # self.calibrator = Calibrator(self.runner)
-    def init(self):
-        pass
-        # self.runner.init()
-    def execute(self):
-        # self.calibrator.run()
-        pass
+
     
     
